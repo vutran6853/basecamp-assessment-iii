226a62f5b2be55def9f446bdb10f99c19a071013
Vu Tran <vutran6853@gmail.com>
2018-07-26 22:26:28 -0600

    Create React App

---
+++ my_app/src/Computer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Computer extends Component {
+    render() {
+        return (
+            <div className="Computer">
+                <h1>Section about Custom PC built!!</h1>
+            </div>
+        )
+    }
+}
+
+export default Computer;
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Computer from './Computer';
+import ShoppingList from './shoppingList';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+ReactDOM.render(<Computer />, document.getElementById('computer'));
+registerServiceWorker();
+
+ReactDOM.render(<ShoppingList />, document.getElementById('shoppingList'));
 registerServiceWorker();
